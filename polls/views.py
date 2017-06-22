89d4d58531da5080b244ddebff637e3f739ca9c4
fusionlaw <liufuxiang1986@gmail.com>
2017-06-22 13:40:15 +0800

    1.添加样式表 2.在index添加css 3.修改后台 4.在模型中添加方法 5.在URLconf中使用通用视图 6.在view中修改vote，使用F()函数控制数据同步

--- polls/views.py
+++ polls/views.py
@@ -4,10 +4,12 @@ from django.http import Http404
 from django.template import loader
 from polls.models import Question, Choice
 from django.urls import reverse
+from django.views import generic
+from django.db.models import F
 # Create your views here.
 
 
-def index(request):
+# def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
 
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -21,12 +23,19 @@ def index(request):
     # }
     # return HttpResponse(template.render(context, request))
 
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
+# def detail(request, question_id):
 
     # return HttpResponse("You're looking at question %s." % question_id)
 
@@ -36,16 +45,25 @@ def detail(request, question_id):
     #     raise Http404("Question does not exist")
     # return render(request, 'polls/detail.html', {'question': question})
 
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+# def results(request, question_id):
     # response = "You're looking at the results of question %s."
     # return HttpResponse(response % question_id)
 
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -59,7 +77,8 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
+        # selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
