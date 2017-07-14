9f583fde8b55c48674f01f3bdf5a60c0e2aeb273
fusionlaw <liufuxiang1986@gmail.com>
2017-07-14 16:47:53 +0800

    社交网站使用内置登录视图

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-from . import views
+from .views import dashboard
+from django.contrib.auth import views
 
 urlpatterns = [
     # post views
-    url(r'^login/$', views.user_login, name='login'),
+    # url(r'^login/$', views.user_login, name='login'),
+    url(r'^$', dashboard, name='dashboard'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^logout-then-login/$', views.logout_then_login, name='logout_then_login'),
 ]
 
